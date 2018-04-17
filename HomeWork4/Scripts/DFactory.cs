1e0bba1e4b73226c2cc7592c41b7069836cdfe78
Xiong <347964202@qq.com>
2018-04-18 00:19:50 +0800

    HomeWork4

---
+++ HomeWork4/Scripts/DFactory.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.Factory;
+namespace Com.Factory{
+	/*新建一个飞碟工厂类，飞碟工厂的作用主要是管理飞碟实例，还需要屏蔽飞碟实例的提取和回收，主要要实现的函数(提供给其他对象调用的函数)如下：
+		1、getDisk 
+		2、getDiskObject
+		3、free
+	*/
+	public class DiskFactory : System.Object{
+		//需要两个东西，一个是工厂类的实例，一个是飞碟队列
+		private static DiskFactory single_instance; 
+		private static List<GameObject> listOfDisk;
+		public GameObject DiskPrefab;
+		// 获取工厂实例
+		public static DiskFactory getInstance(){
+			if (single_instance == null) {
+				single_instance = new DiskFactory ();
+				listOfDisk = new List<GameObject> ();
+			}
+			return single_instance;
+		}
+		// getDisk
+		// 有空闲的飞碟就返回其id，否则就在队列中添加一个新的飞碟对象并返回其id
+		public int getDisk(){
+			for (int i = 0; i < listOfDisk.Count; ++i) {
+				if (!listOfDisk [i].activeInHierarchy) {
+					return i;
+				}
+			}
+			listOfDisk.Add (GameObject.Instantiate (DiskPrefab) as GameObject);
+			listOfDisk [listOfDisk.Count - 1].AddComponent<Rigidbody>();
+			return listOfDisk.Count - 1;
+		}
+		// getDiskObject
+		public GameObject getDiskObject(int i){
+			if (i >= 0 && i < listOfDisk.Count) {
+				return listOfDisk [i];
+			}
+			return null;
+		}
+
+	 	// free
+		public void free(int i){
+			if (i >= 0 && i < listOfDisk.Count) {
+				listOfDisk [i].GetComponent<Rigidbody> ().velocity = Vector3.zero;     // 重置速度
+				listOfDisk [i].transform.localScale = DiskPrefab.transform.localScale; // 重制大小
+				listOfDisk [i].SetActive (false); // 使其不显示　
+			}
+		}
+	}
+}
+public class DFactory : MonoBehaviour {
+	public GameObject disk;
+
+	void Awake(){
+		// 初始化对象
+		DiskFactory.getInstance ().DiskPrefab = disk;
+	}
+}
---
+++ HomeWork4/Scripts/UserInterface.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Com.Factory;
+
+public class UserInterface : MonoBehaviour{
+	/* 用户界面有两大作用，一个是记录用户得分和计时以及显示回合数，一个是处理用户的输入
+	 * 而用户的输入有两个，一个是用户鼠标左键的点击，一个是用户的空格输入
+	 * 每一次用户鼠标左键的点击都会从摄像头发出一条射线，代表了用户射出的子弹，我们只创立一个子弹对象，所以每一次射出子弹之前
+	 * 都要重置子弹的位置并重置子弹的速度，当然施加在子弹上的力是保持不变的
+	 * 飞碟发射就直接调用用户接口
+	 * 得分计时和显示回合数就通过一个查询的接口
+	*/
+	public Text maintips;   // 倒计时和新回合的提示
+	public Text scoresText; // 得分的提示
+	public Text roundText;  // 回合的提示
+	private int roundnow;   // 现在所处于的回合
+	public GameObject bullet; // 子弹
+	public float bulletInterval = 0.25f;   // 发射子弹的间隔
+	public float bulletSpeed = 500f; // 子弹的速度
+	private float nextBulletTime;
+
+	private IUserInterface userInt; // 这是用户接口 
+	private IQueryStatus queryInt;  // 这是查询接口
+
+	void Start(){
+		// 子弹、爆炸碎片、用户接口和查询接口
+		bullet = GameObject.Instantiate(bullet) as GameObject;
+		userInt = SceneController.getInstance () as IUserInterface;
+		queryInt = SceneController.getInstance () as IQueryStatus;
+	}
+
+	void Update(){
+		if (queryInt.isCounting()) {
+			maintips.text = ((int)queryInt.getEmitTime ()).ToString ();  // 显示游戏的倒计时
+		} else {
+			if (Input.GetKeyDown ("space")) {
+				userInt.emitDisk ();    // 有监测到空格按下就发射飞碟
+			}
+			if (queryInt.isShooting ()) {
+				maintips.text = "";     // 一旦开始射击了，表明游戏开始，隐藏游戏提示
+			}
+			if (queryInt.isShooting () && Input.GetMouseButtonDown (0) && Time.time > nextBulletTime) {
+				// 发射子弹
+				nextBulletTime = Time.time + bulletInterval;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); // 由摄像机发出，方向为摄像机到鼠标点击位置的连线
+				bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;     // 速度重置
+				bullet.transform.position = transform.position;               // 子弹从摄像机的位置射出
+				bullet.GetComponent<Rigidbody>().AddForce(bulletSpeed*ray.direction,ForceMode.Impulse);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Disk") {  
+					// 击中飞碟设置为不活跃，自动回收  
+					hit.collider.gameObject.SetActive(false);  
+				}  
+
+			}
+		}
+		roundText.text = "Round: " + queryInt.getRound ().ToString (); // 通过查询接口查询关卡数
+		scoresText.text = "Scores: " + queryInt.getScores ().ToString (); // 通过查询接口查询当前分数
+		if (roundnow != queryInt.getRound ()) {
+			// 如果关卡数更新要在mainTips里面进行提示
+			roundnow = queryInt.getRound();
+			maintips.text = "Round: " + roundnow.ToString () + " ! ";
+		}
+	}
+}
+
